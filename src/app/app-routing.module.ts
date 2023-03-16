85253ef23f084656b6eaf8274012bf94b2488552
glazzerino <fztmon@gmail.com>
2023-03-15 22:02:51 -0600

    text page WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModerationsComponent } from './moderations/moderations.component';
+import { ParentMenuComponent } from './parent-menu/parent-menu.component';
+import { TextdavinciComponent } from './textdavinci/textdavinci.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'text', component: TextdavinciComponent },
+  { path: 'moderations', component: ModerationsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+imports: [BrowserModule, NgModule, AppRoutingModule,]
 import { AppComponent } from './app.component';
+import { ParentMenuComponent } from './parent-menu/parent-menu.component';
+import { TextdavinciComponent } from './textdavinci/textdavinci.component';
+import { ModerationsComponent } from './moderations/moderations.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentMenuComponent,
+    TextdavinciComponent,
+    ModerationsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-
+export class AppModule {
 }
