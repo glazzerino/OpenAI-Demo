99b67cfbb72e274fbef1f1217a02e70ce62824fd
glazzerino <fztmon@gmail.com>
2023-03-20 02:25:03 -0600

    env swapping WIP

--- src/app/poems/poems.component.ts
+++ src/app/poems/poems.component.ts
@@ -1,4 +1,10 @@
 import { DavinciService } from "../davinci.service";
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-poems',
+  templateUrl: './poems.component.html',
+  styleUrls: ['./poems.component.css']
+})
 export class PoemsComponent {
   input: string = "";
   constructor(private openaiService: DavinciService) { };
